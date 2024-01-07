5051697325cac7863dbcaf2a46ebbee3dee9e834
wrightkhlebisol <calebogundiya@gmail.com>
2024-01-07 21:54:59 +0000

    status endpoint

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """AirBNB Flask API"""
+from api.v1.views import app_views
 from flask import Flask
 from models import storage
 from os import getenv
@@ -9,16 +10,13 @@ host = getenv('HBNB_API_HOST', '0.0.0.0'),
 port = getenv('HBNB_API_PORT', 5000)
 
 app = Flask(__name__)
+app.register_blueprint(app_views)
 
-@app.route('/api/v1/status')
-def status():
-    return {
-        "status": "OK"
-    }
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host, port)
-
-
-
+    app.run(host=host, port=port, threaded=True)
