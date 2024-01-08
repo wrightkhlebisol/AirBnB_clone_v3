ca422063ba3736b2cfa413c5a5225bb79a4a2d35
wrightkhlebisol <calebogundiya@gmail.com>
2024-01-08 15:37:45 +0000

    fix plural classes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,12 +11,12 @@ from models.state import State
 from models.user import User
 
 classes = {
-    "Amenity": Amenity,
-    "City": City,
-    "Place": Place,
-    "Review": Review,
-    "State": State,
-    "User": User
+    "amenities": Amenity,
+    "Cities": City,
+    "Places": Place,
+    "Reviews": Review,
+    "States": State,
+    "Users": User
 }
 
 
