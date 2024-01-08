1d8ed08bbb097ee406ac177bc79d182732f34409
wrightkhlebisol <calebogundiya@gmail.com>
2024-01-08 00:55:09 +0000

    get stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,35 @@
 #!/usr/bin/python3
 """Blueprint views"""
-
 from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {
+    "Amenity": Amenity,
+    "City": City,
+    "Place": Place,
+    "Review": Review,
+    "State": State,
+    "User": User
+}
 
 
 @app_views.route('/status')
 def status():
-    return {
-        "status": "OK"
-    }
+    return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats')
+def stats():
+    stats = {}
+
+    for k, v in classes.items():
+        stats[k.lower()] = storage.count(v)
+
+    return jsonify(stats)
