393328585ac0936137719e3533a520f039b1519c
wrightkhlebisol <calebogundiya@gmail.com>
2024-01-10 02:10:48 +0000

    delete password

--- models/base_model.py
+++ models/base_model.py
@@ -21,6 +21,7 @@ else:
 
 class BaseModel:
     """The BaseModel class from which future classes will be derived"""
+
     if models.storage_t == "db":
         id = Column(String(60), primary_key=True)
         created_at = Column(DateTime, default=datetime.utcnow)
@@ -28,18 +29,22 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initialization of the base model"""
+
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
                     setattr(self, key, value)
+
             if kwargs.get("created_at", None) and type(self.created_at) is str:
                 self.created_at = datetime.strptime(kwargs["created_at"], time)
             else:
                 self.created_at = datetime.utcnow()
+
             if kwargs.get("updated_at", None) and type(self.updated_at) is str:
                 self.updated_at = datetime.strptime(kwargs["updated_at"], time)
             else:
                 self.updated_at = datetime.utcnow()
+
             if kwargs.get("id", None) is None:
                 self.id = str(uuid.uuid4())
         else:
@@ -49,6 +54,7 @@ class BaseModel:
 
     def __str__(self):
         """String representation of the BaseModel class"""
+
         return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
                                          self.__dict__)
 
@@ -61,13 +67,20 @@ class BaseModel:
     def to_dict(self):
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
+
         if "created_at" in new_dict:
             new_dict["created_at"] = new_dict["created_at"].strftime(time)
+
         if "updated_at" in new_dict:
             new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
         new_dict["__class__"] = self.__class__.__name__
+
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
+
+        if "password" in new_dict:
+            del new_dict["password"]
+
         return new_dict
 
     def delete(self):
